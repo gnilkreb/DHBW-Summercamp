5290f91e4cde0fd11a582a290964aa11f80c16bf
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-07 16:30:17 +0200

    Include app.css in styles

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" src="/components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/app.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 
 @stack('styles')
