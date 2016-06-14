ba0236308ea0c5fb496a680ee63ee0f676032ad5
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-14 13:26:04 +0200

    Refactor login and register  templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,44 +1,27 @@
-@extends('layouts.app')
+@extends('auth.partial')
 
-@section('body')
-    <div class="centered-logo">
-        <object class="logoobject" data="img/svg/logo.svg" type="image/svg+xml"></object>
-    </div>
+@section('title', 'Anmelden')
 
-    <div class="container landing-container">
-        <hr/>
-        <div class="col-md-6 col-md-offset-3 sc-panel scratch-panel">
+@section('auth-content')
+    <form style="margin: 25px;">
+        <div class="form-group">
+            <label for="first_name">Vorname</label>
+            <input type="text" class="form-control" id="first_name" placeholder="Vorname">
+        </div>
+        <div class="form-group">
+            <label for="last_name">Nachname</label>
+            <input type="text" class="form-control" id="last_name" placeholder="Nachname">
+        </div>
+        <div class="form-group">
             <div class="row">
-                <div class="col-xs-12 content-top-border"></div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 content">
-                    <form style="margin: 25px;">
-                        <div class="form-group">
-                            <label for="first_name">Vorname</label>
-                            <input type="text" class="form-control" id="first_name" placeholder="Vorname">
-                        </div>
-                        <div class="form-group">
-                            <label for="last_name">Nachname</label>
-                            <input type="text" class="form-control" id="last_name" placeholder="Nachname">
-                        </div>
-                        <div class="form-group">
-                            <div class="row">
-                                <div class="col-xs-6" style="text-align: left;">
-                                    <a href="index.html"><img src="img/svg/arrow_back.svg" width="25%" class="hvr-grow"></a>
-                                </div>
-                                <div class="col-xs-6" style="text-align: right;">
-                                    <input type="image" src="img/svg/savedata_green.svg" alt="Submit" width="25%"
-                                           class="hvr-grow">
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+                <div class="col-xs-6" style="text-align: left;">
+                    <a href="/"><img src="img/svg/arrow_back.svg" width="25%" class="hvr-grow"></a>
+                </div>
+                <div class="col-xs-6" style="text-align: right;">
+                    <input type="image" src="img/svg/savedata_green.svg" alt="Submit" width="25%"
+                           class="hvr-grow">
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xs-12 content-bottom-border"></div>
-            </div>
         </div>
-    </div>
+    </form>
 @endsection
---
+++ resources/views/auth/partial.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="centered-logo">
+        <object class="logoobject" data="img/svg/logo.svg" type="image/svg+xml"></object>
+    </div>
+
+    <div class="container landing-container">
+        <hr/>
+        <div class="col-md-6 col-md-offset-3 sc-panel scratch-panel">
+            <div class="row">
+                <div class="col-xs-12 content-top-border"></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 content">
+                    @yield('auth-content')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 content-bottom-border"></div>
+            </div>
+        </div>
+    </div>
+@endsection
