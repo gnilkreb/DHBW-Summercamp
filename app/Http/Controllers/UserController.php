6c37c388dc3c1cd1b74a727a14923c5b2ced2c02
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-14 14:38:29 +0200

    Implemented Authenticatable contract on user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Requests\UserRegisterRequest;
 use App\User;
+use Auth;
 
 class UserController extends Controller
 {
@@ -12,6 +13,10 @@ class UserController extends Controller
     public function register(UserRegisterRequest $request)
     {
         $user = User::create($request->all());
+
+        Auth::login($user);
+
+        return redirect('categories');
     }
     
 }
--- app/User.php
+++ app/User.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
 
     protected $fillable = ['first_name', 'last_name', 'age', 'gender'];
@@ -12,6 +13,67 @@ class User extends Model
     public function team()
     {
         return $this->hasOne('App/Team');
+    }
+
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        // TODO: Implement getRememberToken() method.
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        // TODO: Implement setRememberToken() method.
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        // TODO: Implement getRememberTokenName() method.
     }
 
 }
