3a1533eae225bfdbb6defde226ac8533eb6e9b86
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-28 20:24:42 +0200

    Installed Carbon and worked on admin users page

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -3,5 +3,26 @@
 @section('title', 'Benutzer')
 
 @section('content')
-    Benutzer
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Alter</th>
+                <th>Geschlecht</th>
+                <th>Registriert seit</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->first_name . ' ' . $user->last_ame }}</td>
+                    <td>{{ $user->age }}</td>
+                    <td>{{ $user->gender }}</td>
+                    <td>{{ $user->created_at }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
