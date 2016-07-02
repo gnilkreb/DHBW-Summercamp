db36c254cf86410b471e810f6d932308a73bb0cc
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-02 15:42:08 +0200

    Create migration to remove youtube url and file id from tasks table

---
+++ database/migrations/2016_07_02_133954_remove_youtube_and_file_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveYoutubeAndFileColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('youtube_url');
+            $table->dropColumn('file_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('youtube_url')->nullable();
+            $table->integer('file_id')->unsigned()->nullable();
+        });
+    }
+}
