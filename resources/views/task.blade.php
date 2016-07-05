81ba18045ed01c9ec8e0ef7e40b453251a1b66e0
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-05 15:30:34 +0200

    Insert content and youtube url into task view

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -14,7 +14,22 @@
                 <div class="col-xs-12 content-top-border-12"></div>
             </div>
             <div class="row">
-                <div class="col-xs-12 content">{!! $task->content !!}</div>
+                <div class="col-xs-12 content">
+                    <div class="col-xs-6">
+                        <p style="text-align: left; margin-left: 25px; font-size: 2rem; letter-spacing: 0.25rem; line-height: 3rem;">{!! $task->content !!}</p>
+                    </div>
+                    <div class="col-xs-6">
+                        <!-- https://www.youtube.com/embed/8JG4aeVreCQ?rel=0&amp;showinfo=0 -->
+                        <div style="margin-right: 25px;">
+                            <iframe id="ytplayer" type="text/html" width="100%" height="390"
+                                    style="background-color: white;"
+                                    src="{{ $task->youtube_url }}"
+                                    frameborder="0"
+                                    scrolling="no"></iframe>
+                        </div>
+
+                    </div>
+                </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 content-bottom-border-12"></div>
