190ca0e123b98ee2ea840fab73f00d2c0837d8eb
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-05 15:25:43 +0200

    Create migration for adding youtube url to tasks

---
+++ database/migrations/2016_07_05_132449_add_youtube_url_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddYoutubeUrlColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('youtube_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('youtube_url');
+        });
+    }
+}
