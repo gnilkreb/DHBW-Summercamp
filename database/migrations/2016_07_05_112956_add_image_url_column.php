5d32e9282e99c638ad3337de01b176527167fae6
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-05 13:30:57 +0200

    Create AddImageUrlColumn migration

---
+++ database/migrations/2016_07_05_112956_add_image_url_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImageUrlColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('image_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('image_url');
+        });
+    }
+}
