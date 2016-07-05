9c87ae3c795c960d6563a554db8101baf04280b8
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-05 15:37:17 +0200

    Import fonts and svg css into sass

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -4,8 +4,6 @@
 <link rel="stylesheet" href="/components/hover/css/hover-min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="/css/fonts.css">
-<link rel="stylesheet" href="/css/svg.css">
 <link rel="stylesheet" href="/css/app.css">
 
 @stack('styles')
