daa28ca0148838569de697beb8b6c2d4a85d7115
Philipp <PhilippSchemel@live.com>
2016-07-06 07:45:00 +0200

    Automatic movement - so smooth, so god damn smooth!

--- resources/assets/js/parallax.js
+++ resources/assets/js/parallax.js
@@ -1,16 +1,18 @@
 var landscapeX = 0;
 var cloudsX = 0;
 var mouseX = 0;
-var speed = 1;
+var speed = 0.5;
 var parafactor = 2;
 
 $('.bg-landscape').css('background-position', landscapeX + 'px bottom');
 $('.bg-clouds').css('background-position', cloudsX + 'px bottom');
 
+update();
+
 $(document).mousemove(function(event) {
       if(mouseX != 0) {
         if(event.pageX > mouseX) {
-          landscapeX = landscapeX - speed * parafactor;
+          landscapeX = landscapeX - (speed * parafactor);
           cloudsX = cloudsX - speed;
         }
         if(event.pageX < mouseX) {
@@ -24,3 +26,12 @@ $(document).mousemove(function(event) {
 
       mouseX = event.pageX;
 });
+
+function update() {
+  setInterval(function() {
+    landscapeX = landscapeX + speed * parafactor;
+    cloudsX = cloudsX + speed;
+    $('.bg-landscape').css('background-position', landscapeX + 'px bottom');
+    $('.bg-clouds').css('background-position', cloudsX + 'px bottom');
+  }, 50);
+}
