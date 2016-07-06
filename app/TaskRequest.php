99fb80909ad1ce95e10150e9d07f5b7ab6dced13
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-06 10:50:54 +0200

    Implement TaskRequest model

--- app/TaskRequest.php
+++ app/TaskRequest.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskRequest extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'task_id',
+        'done'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+
+    public function task()
+    {
+        return $this->hasOne('App\Task');
+    }
+
 }
