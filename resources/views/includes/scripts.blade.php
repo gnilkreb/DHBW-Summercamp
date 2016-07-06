b296f5ae6f7ae56114c87278fede27f69cda7334
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-06 16:23:28 +0200

    Include google analytics

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -8,5 +8,14 @@
 <script src="/components/pusher-websocket-iso/dist/web/pusher.js"></script>
 <script src="/components/notie/dist/notie.min.js"></script>
 <script src="/js/app.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-40279069-5', 'auto');
+    ga('send', 'pageview');
+</script>
 
 @stack('scripts')
