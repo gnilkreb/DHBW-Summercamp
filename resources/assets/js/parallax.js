9f6f37779aea44cc46061663f8eb9c7a1418aafb
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-06 15:10:16 +0200

    Smooth parallax

--- resources/assets/js/parallax.js
+++ resources/assets/js/parallax.js
@@ -1,7 +1,7 @@
 var landscapeX = 0;
 var cloudsX = 0;
 var mouseX = 0;
-var speed = 1;
+var speed = 0.1;
 var parafactor = 2;
 var direction = 1;
 
@@ -10,30 +10,31 @@ $('.bg-clouds').css('background-position', cloudsX + 'px bottom');
 
 update();
 
-$(document).mousemove(function(event) {
-      if(mouseX != 0) {
-        if(event.pageX > mouseX) {
-          direction = -1;
+$(document).mousemove(function (event) {
+    if (mouseX != 0) {
+        if (event.pageX > mouseX) {
+            direction = -1;
         }
-        if(event.pageX < mouseX) {
-          direction = 1;
+        if (event.pageX < mouseX) {
+            direction = 1;
         }
-      }
+    }
 
-      landscapeX = landscapeX + (direction * (speed * parafactor));
-      cloudsX = cloudsX + (direction * speed);
+    landscapeX = landscapeX + (direction * (speed * parafactor));
+    cloudsX = cloudsX + (direction * speed);
 
-      $('.bg-landscape').css('background-position', landscapeX + 'px bottom');
-      $('.bg-clouds').css('background-position', cloudsX + 'px bottom');
+    $('.bg-landscape').css('background-position', landscapeX + 'px bottom');
+    $('.bg-clouds').css('background-position', cloudsX + 'px bottom');
 
-      mouseX = event.pageX;
+    mouseX = event.pageX;
 });
 
 function update() {
-  setInterval(function() {
-    landscapeX = landscapeX + (direction * (speed * parafactor));
-    cloudsX = cloudsX + (direction * speed);
-    $('.bg-landscape').css('background-position', landscapeX + 'px bottom');
-    $('.bg-clouds').css('background-position', cloudsX + 'px bottom');
-  }, 50);
+    setInterval(function () {
+        landscapeX = landscapeX + (direction * (speed * parafactor));
+        cloudsX = cloudsX + (direction * speed);
+
+        $('.bg-landscape').css('background-position', Math.round(landscapeX) + 'px bottom');
+        $('.bg-clouds').css('background-position', Math.round(cloudsX) + 'px bottom');
+    }, 1);
 }
