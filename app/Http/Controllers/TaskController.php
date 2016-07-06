b05d8eec63b54e5e997659d86579718051c4dddd
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-06 14:33:54 +0200

    Implement task request creation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -19,7 +19,12 @@ class TaskController extends BaseController
 
     public function request($id, Request $request)
     {
-        
+        Task::create([
+            'user_id' => $request->user()->id,
+            'task_id' => $id
+        ]);
+
+        return response(200);
     }
 
 }
