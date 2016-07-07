aa69545f5d606fd3d74f1c50ca13781126845e1e
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-07 09:29:26 +0200

    Implement create answers table migration

---
+++ database/migrations/2016_07_07_070816_create_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('task_id')->unsigned();
+            $table->string('label');
+            $table->boolean('correct')->default(false);
+            $table->timestamps();
+
+            $table->foreign('task_id')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('answers');
+    }
+}
