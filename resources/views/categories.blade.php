3e2f533bfc9525f738cd3f15ad242dbc17d42637
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-08 11:41:04 +0200

    Hide category if it is not active

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -10,21 +10,23 @@
     <div class="container landing-container">
         <hr/>
         @foreach($categories as $index => $category)
-            <div class="col-xs-5 @if($index >= 1) col-xs-offset-2 @endif sc-panel scratch-panel">
-                <div class="row">
-                    <div class="col-xs-12 content-top-border-6"></div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12 content">
-                        <h2>{{ $category->name }}</h2>
-                        <a href="/category/{{ $category->id }}"><img src="{{ $category->image_url }}" width="222px"
-                                         class="img-responsive center-block hvr-grow sc-start-image"></a>
+            @if($category->active)
+                <div class="col-xs-5 @if($index >= 1) col-xs-offset-2 @endif sc-panel scratch-panel">
+                    <div class="row">
+                        <div class="col-xs-12 content-top-border-6"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 content">
+                            <h2>{{ $category->name }}</h2>
+                            <a href="/category/{{ $category->id }}"><img src="{{ $category->image_url }}" width="222px"
+                                             class="img-responsive center-block hvr-grow sc-start-image"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 content-bottom-border-6"></div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-xs-12 content-bottom-border-6"></div>
-                </div>
-            </div>
+            @endif
         @endforeach
     </div>
 @endsection
