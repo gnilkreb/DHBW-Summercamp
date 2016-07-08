0708c41e49f73c48c5fdb4e25216f942ca4a8509
Marc Vornetran <marc.vornetran@gmail.com>
2016-07-08 11:10:35 +0200

    Install c3 and d3 for statistics view

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -5,3 +5,12 @@
 @section('content')
     Statistik
 @endsection
+
+@push('styles')
+    <link rel="stylesheet" href="/components/c3/c3.min.css">
+@endpush
+
+@push('scripts')
+    <script src="/components/d3/d3.min.js"></script>
+    <script src="/components/c3/c3.min.js"></script>
+@endpush
