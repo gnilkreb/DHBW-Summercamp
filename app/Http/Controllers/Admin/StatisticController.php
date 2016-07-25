560e62a69b35885e9ed219cda774305d4e464874
schempil <PhilippSchemel@live.com>
2016-07-26 01:26:45 +0200

    Piechart works :pineapple:

--- app/Http/Controllers/Admin/StatisticController.php
+++ app/Http/Controllers/Admin/StatisticController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\User;
+
 class StatisticController extends BaseController
 {
 
     public function index()
     {
-        return $this->adminView('statistics');
+        return $this->adminView('statistics', ['users' => $users = User::all()]);
     }
 
 }
--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -6,14 +6,15 @@
     <div class="alert alert-warning">
         <strong>TODO:</strong> Richtige statt Guttenberg Daten verwenden
     </div>
+    <div id="users" data-users-array="{{$users}}" ></div>
     <div id="statistics-container" class="container">
         <div class="col-md-4">
             <h2>Geschlechtsverteilung</h2>
-            <div id="piechart-gender"></div>
+            <div id="piechart-gendero"></div>
         </div>
         <div class="col-md-8">
             <h2>Altersverteilung</h2>
-            <div id="barchart-age"></div>
+            <div id="barchart-ageo"></div>
         </div>
     </div>
 @endsection
@@ -25,4 +26,55 @@
 @push('scripts')
     <script src="/components/d3/d3.min.js"></script>
     <script src="/components/c3/c3.min.js"></script>
+    <script>
+        var users = $('#users').data('users-array');
+
+        var male = 0;
+        var female = 0;
+
+        for(var i = 0; i < users.length; i++) {
+            if(users[i].gender == 1) {
+                male++;
+            } else {
+                female++;
+            }
+        }
+
+        const piechartGender = c3.generate({
+            bindto: '#piechart-gendero',
+            data: {
+                // iris data from R
+                columns: [
+                    ['Männlich', male],
+                    ['Weiblich', female],
+                ],
+                type: 'donut'
+            },
+            donut: {
+                title: 'Geschlechtsverteilung'
+            }
+        });
+
+        const barchartAge = c3.generate({
+            bindto: '#barchart-ageo',
+            data: {
+                columns: [
+                    ['Männlich', 13, 17, 13, 14, 12, 13, 16, 9],
+                    ['Weiblich', 15, 15, 17, 16, 11, 15, 18, 11],
+                    ['Gesamt', 13, 17, 13, 14, 12, 13, 16, 9, 15, 15, 17, 16, 11, 15, 18, 11]
+                ],
+                type: 'bar'
+            },
+            bar: {
+                width: {
+                    ratio: 0.5
+                }
+            },
+            axis: {
+                x: {
+                    categories: ['Category 1', 'Category 2']
+                }
+            }
+        });
+    </script>
 @endpush
