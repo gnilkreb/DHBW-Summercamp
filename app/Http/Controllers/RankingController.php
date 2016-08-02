399a71bfa676af20beb90352b511725ee14f73d3
Marc Vornetran <marc.vornetran@gmail.com>
2016-08-02 22:48:06 +0200

    RankingController should extend BaseController for middleware shizzle

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -3,24 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 
-class RankingController extends Controller
+class RankingController extends BaseController
 {
-    public function index() {
+
+    public function index()
+    {
         $activeUserId = Auth::user()->id;
         $users = User::where('role', 'user')->get();
-        foreach($users as $user) {
+
+        foreach ($users as $user) {
             $user->stars = $user->getStars();
             $user->rndm = rand(10, 30);
         }
 
         $ranking = $users->sortByDesc('stars');
 
-
-        return view('ranking', ['activeUserId' => $activeUserId, 'users' => $ranking]);
+        return view('ranking', [
+            'activeUserId' => $activeUserId,
+            'users' => $ranking
+        ]);
     }
+
 }
