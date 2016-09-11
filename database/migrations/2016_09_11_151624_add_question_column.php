269ea5813534e6de8ffee0be0984fd3f59c9cd83
Marc Vornetran <marc.vornetran@gmail.com>
2016-09-11 17:17:40 +0200

    Migration to add question column to tasks table

---
+++ database/migrations/2016_09_11_151624_add_question_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddQuestionColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('question')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('question');
+        });
+    }
+}
